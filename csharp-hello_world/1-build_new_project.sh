ef80c603d08d139811235c9e44c392e2ec862f91
mutabazi-bruno <b.mutabazi@gmail.com>
2026-01-12 11:53:21 +0200

    build

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet new console -n 1-new_project > /dev/null
-dotnet build 1-new_project | tail -5 | head -3
+dotnet new console -n 1-new_project -o 1-new_project &> /dev/null
+dotnet build 1-new_project -v q --nologo | grep -A 2 "Build succeeded"
