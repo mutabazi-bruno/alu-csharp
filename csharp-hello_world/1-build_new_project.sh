7bc5126964840287c22bf29c42537f953f68a390
mutabazi-bruno <b.mutabazi@gmail.com>
2026-01-14 11:10:43 +0200

    add new

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Script to initialize and build a new C# project
 DIR="1-new_project"
 
 if [ -d "$DIR" ]; then
---
+++ csharp-hello_world/2-run_new_project.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+dotnet new console -o 2-new_project
+dotnet build 2-new_project
+dotnet run --project 2-new_project
