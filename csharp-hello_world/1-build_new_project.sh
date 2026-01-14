72757e46cd707404628a0e4f22e8ea6ed21377f0
mutabazi-bruno <b.mutabazi@gmail.com>
2026-01-14 10:51:44 +0200

    add neww

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dotnet new console -n 1-new_project -o 1-new_project &> /dev/null
-dotnet build 1-new_project -v q --nologo | grep -A 2 "Build succeeded"
+dotnet new console -n 1-new_project -o 1-new_project
+dotnet build 1-new_project
