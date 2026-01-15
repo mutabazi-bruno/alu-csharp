c7fb0480e2130f00c3e3cd3522eceff85f7de10c
mutabazi-bruno <b.mutabazi@gmail.com>
2026-01-15 08:46:55 +0200

    add neww

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		str1 = $"Welcome to {str1} {str2}!";
+		Console.WriteLine(str1);
+	}
+}
