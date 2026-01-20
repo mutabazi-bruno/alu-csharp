090d72fbc544fc4e9e7456655b8425731a57c124
mutabazi-bruno <b.mutabazi@gmail.com>
2026-01-20 08:48:31 +0200

    if

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+        }
+    }
+}
