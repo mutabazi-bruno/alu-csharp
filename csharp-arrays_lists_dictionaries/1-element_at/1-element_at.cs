329a45065d0ecbb971915a4ea62d7722e854ba46
mutabazi-bruno <b.mutabazi@gmail.com>
2026-01-22 09:59:56 +0200

    array

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,25 +1,15 @@
 using System;
-using System.Collections.Generic;
 
-/// <summary>
-/// Array class
-/// </summary>
-class Array
+public class Array
 {
-    /// <summary>
-    /// Retrieves an element from an array at a given position
-    /// </summary>
-    /// <param name="array">The array</param>
-    /// <param name="index">The index position</param>
-    /// <returns>The element at the given index, or -1 if index is out of range</returns>
-    public static int elementAt(List<int> myList, int index)
+    public static int elementAt(int[] array, int index)
     {
-        if (index < 0 || index >= myList.Count)
+        if (index < 0 || index >= array.Length)
         {
             Console.WriteLine("Index out of range");
             return -1;
         }
 
-        return myList[index];
+        return array[index];
     }
 }
--- csharp-arrays_lists_dictionaries/1-element_at/1-main.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-main.cs
@@ -1,12 +1,11 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> myList = new List<int>() {1, 2, 3, 4, 5};
+        int[] myArray = {1, 2, 3, 4, 5};
 
-        Console.WriteLine(Array.elementAt(myList, 3));
+        Console.WriteLine(Array.elementAt(myArray, 3));
     }
 }
