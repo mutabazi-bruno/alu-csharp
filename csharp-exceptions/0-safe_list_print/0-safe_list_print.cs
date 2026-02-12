711a79da99b0619f86b2c09b096a6b1f7c2560cc
mutabazi-bruno <b.mutabazi@gmail.com>
2026-02-12 08:24:58 +0200

    exception

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                count++;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            // Silently catch the exception when accessing beyond list bounds
+        }
+
+        return count;
+    }
+}
