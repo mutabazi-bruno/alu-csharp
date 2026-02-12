b3bcfb1efd3e0b98b881061a8d42104f93870bdb
mutabazi-bruno <b.mutabazi@gmail.com>
2026-02-12 08:53:57 +0200

    S/E

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,16 @@
+using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
