95e455c8f199f3163d6e8a67bada767be404caf0
mutabazi-bruno <b.mutabazi@gmail.com>
2026-02-21 11:46:04 +0200

    class

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defines a zombie with a public health field.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Health of the zombie.</summary>
+        public int health;
+
+        /// <summary>Initializes a new Zombie with 0 health.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new Zombie with specified health.
+        /// </summary>
+        /// <param name="value">The health value.</param>
+        /// <exception cref="ArgumentException">Thrown when value is less than 0.</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}
