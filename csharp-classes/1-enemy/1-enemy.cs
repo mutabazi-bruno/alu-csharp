9928a3a5c05bfebc859acf707d970165ad292e1e
mutabazi-bruno <b.mutabazi@gmail.com>
2026-02-21 11:40:41 +0200

    class

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defines a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health field.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Constructor that sets health to 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
