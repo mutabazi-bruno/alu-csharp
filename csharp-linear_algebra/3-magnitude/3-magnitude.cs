165ccfef95b0c62d0311210fee371367f092d724
mutabazi-bruno <b.mutabazi@gmail.com>
2026-03-11 10:42:57 +0200

    la

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -2,6 +2,13 @@ using System;
 
 public class VectorMath
 {
+    /// <summary>
+    /// Calculates and returns the length of a given vector.
+    /// The vector can be 2D or 3D. If not, returns -1.
+    /// The return value is rounded to the nearest hundredth.
+    /// </summary>
+    /// <param name="vector">Array representing the vector.</param>
+    /// <returns>Length of the vector or -1 if invalid.</returns>
     public static double Magnitude(double[] vector)
     {
         if (vector == null || (vector.Length != 2 && vector.Length != 3))
