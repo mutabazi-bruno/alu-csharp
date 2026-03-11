ed1dd71436a80759feea170c40f0f0e22d000fe5
mutabazi-bruno <b.mutabazi@gmail.com>
2026-03-11 11:08:40 +0200

    la

--- csharp-linear_algebra/3-magnitude/obj/Debug/netcoreapp2.1/3-magnitude.AssemblyInfo.cs
+++ csharp-linear_algebra/3-magnitude/obj/Debug/netcoreapp2.1/3-magnitude.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-magnitude")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0addb1d4ca9012b28f06cd273bd9c5b70c2517d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-magnitude")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-magnitude")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
