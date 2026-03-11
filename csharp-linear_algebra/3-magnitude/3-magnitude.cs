190dda154adc162b1eb18eeb73c8aca87868b4c0
mutabazi-bruno <b.mutabazi@gmail.com>
2026-03-11 10:19:01 +0200

    la

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,31 +1,19 @@
 using System;
 
-class VectorMath
+public class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        if (vector.Length == 2)
-        {
-            double result = Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
-            return Math.Round(result, 2);
-        }
-        else if (vector.Length == 3)
-        {
-            double result = Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
-            return Math.Round(result, 2);
-        }
-        else
-        {
+        if (vector == null || (vector.Length != 2 && vector.Length != 3))
             return -1;
+
+        double sumSquares = 0;
+        foreach (double component in vector)
+        {
+            sumSquares += component * component;
         }
-    }
 
-    static void Main(string[] args)
-    {
-        double[] v2D = { 3, 9 };
-        double[] v3D = { 7, -3, -9 };
-
-        Console.WriteLine(Magnitude(v2D));   // 9.49
-        Console.WriteLine(Magnitude(v3D));   // 11.79
+        double length = Math.Sqrt(sumSquares);
+        return Math.Round(length, 2);
     }
 }
