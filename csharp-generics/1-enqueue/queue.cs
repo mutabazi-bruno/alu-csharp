aa26538f7266b5998139669717c6f54a06420b1d
mutabazi-bruno <b.mutabazi@alustudent.com>
2026-06-16 16:52:56 +0200

    1-enqueud

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Queue<T>
+{
+    public class Node
+    {
+        public T value;
+        public Node? next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public Node? head;
+    public Node? tail;
+    public int count;
+
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (count == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail!.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
