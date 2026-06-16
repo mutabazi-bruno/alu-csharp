8dfe8be0e3db9932d4c42d0c1b5d694f84b5b0f5
mutabazi-bruno <b.mutabazi@alustudent.com>
2026-06-16 16:36:35 +0200

    queue

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,35 +1,9 @@
 using System;
 
-/// <summary>
-/// A generic Queue class that provides basic type checking functionality.
-/// </summary>
-/// <typeparam name="T">The type of elements stored in the queue.</typeparam>
-class Queue<T>
+public class Queue<T>
 {
-    /// <summary>
-    /// Gets the name of the type T.
-    /// </summary>
-    /// <returns>The full type name as a string (e.g., "System.String", "System.Double").</returns>
-    public string CheckType()
+    public Type CheckType()
     {
-        return typeof(T).FullName;
-    }
-}
-
-/// <summary>
-/// Main program class for testing the Queue generic class.
-/// </summary>
-class Program
-{
-    /// <summary>
-    /// Main entry point of the application.
-    /// </summary>
-    static void Main(string[] args)
-    {
-        Queue<string> myStrQ = new Queue<string>();
-        Queue<double> myDoubleQ = new Queue<double>();
-
-        Console.WriteLine($"{nameof(myStrQ)} Queue Type: " + myStrQ.CheckType());
-        Console.WriteLine($"{nameof(myDoubleQ)} Queue Type: " + myDoubleQ.CheckType());
+        return typeof(T);
     }
 }
