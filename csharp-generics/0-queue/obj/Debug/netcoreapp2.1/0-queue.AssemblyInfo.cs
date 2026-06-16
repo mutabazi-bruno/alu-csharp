1b8954e78a84294fb586e4898c985b96bd418b76
mutabazi-bruno <b.mutabazi@alustudent.com>
2026-06-16 16:42:07 +0200

    queue

--- csharp-generics/0-queue/obj/Debug/netcoreapp2.1/0-queue.AssemblyInfo.cs
+++ csharp-generics/0-queue/obj/Debug/netcoreapp2.1/0-queue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0-queue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dfe8be0e3db9932d4c42d0c1b5d694f84b5b0f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d31c2c37b9ccf58be2a0d9d30b58a14cbb717c24")]
 [assembly: System.Reflection.AssemblyProductAttribute("0-queue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0-queue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
