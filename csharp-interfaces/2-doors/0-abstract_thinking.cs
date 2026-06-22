c267ed8385f7d93d519911c2dc6e8ba06ec9290c
mutabazi-bruno <b.mutabazi@alustudent.com>
2026-06-22 14:00:14 +0200

    2-door

---
+++ csharp-interfaces/2-doors/0-abstract_thinking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public abstract class Base
+{
+    // Initialize to avoid CS8618 warning
+    public string name { get; set; } = "";
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
