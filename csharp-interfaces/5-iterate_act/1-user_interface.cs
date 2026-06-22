9759f4a31d180ff336a2c7ecfe8fc0a2eacf3177
mutabazi-bruno <b.mutabazi@alustudent.com>
2026-06-22 14:06:47 +0200

    5-iterate

---
+++ csharp-interfaces/5-iterate_act/1-user_interface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+        // intentionally empty
+    }
+
+    public void Break()
+    {
+        // intentionally empty
+    }
+
+    public void Collect()
+    {
+        // intentionally empty
+    }
+}
